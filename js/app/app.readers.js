c9d5d64f9627fa10bef184318cc3e274b84b1bcd
kchapelier <kevin.chapelier@gmail.com>
2014-07-13 00:53:17 +0200

    naive implementation of twoRepeats, vinylStart and vinylStop readers

--- js/app/app.readers.js
+++ js/app/app.readers.js
@@ -26,6 +26,43 @@
 			}
 
 			return sampleValue;
+		},
+		twoRepeats : function(buffer, channel, position, speed, samplesPerSegment) {
+			var sampleValue = 0;
+
+			position = (position % (samplesPerSegment / 2)) * speed; //TODO does it works ?
+
+			if(buffer.length > position) {
+				sampleValue = buffer.getSample(position, channel);
+			}
+
+			return sampleValue;
+		},
+		vinylStart : function(buffer, channel, position, speed, samplesPerSegment) {
+			var sampleValue = 0;
+
+			var ratio = position / samplesPerSegment;
+
+			position = Math.sqrt(ratio) * samplesPerSegment * speed; //TODO does it works ?
+
+			if(buffer.length > position) {
+				sampleValue = buffer.getSample(position, channel);
+			}
+
+			return sampleValue;
+		},
+		vinylStop : function(buffer, channel, position, speed, samplesPerSegment) {
+			var sampleValue = 0;
+
+			var ratio = position / samplesPerSegment;
+
+			position = Math.pow(1 - ratio, 2) * samplesPerSegment * speed; //TODO does it works
+
+			if(buffer.length > position) {
+				sampleValue = buffer.getSample(position, channel);
+			}
+
+			return sampleValue;
 		}
 	};
 
