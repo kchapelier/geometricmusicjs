86bc46b24b44b7b0df7c6f38b8cb9727d052902a
kchapelier <kevin.chapelier@gmail.com>
2014-07-12 12:25:23 +0200

    first Shape implementation

--- js/app/app.js
+++ js/app/app.js
@@ -3,9 +3,12 @@
 var App = App || {};
 
 var ac = Aural.Utils.Support.getAudioContext();
+var s;
 
 window.addEventListener('load', function() {
 	console.log('"I\'m loaded now" says', window, '.');
 	console.log(ac, 'is an AudioContext.');
 	console.log(document.body, 'is empty.');
+
+	console.log(s = new App.Shape(1, 3, null));
 });
--- js/app/app.shape.js
+++ js/app/app.shape.js
@@ -1,7 +1,47 @@
 (function(App) {
 	"use strict";
 
-	var Shape = null;
+	var arrayResize = function(array, size) {
+		var delta = size - array.length;
+		if(delta > 0) {
+			array.length = size; //force the size of the array
+		} else {
+			array.splice(size, -delta); //remove {delta} excedentary elements
+		}
+	};
+
+	var Shape = function(size, segmentsNumber, audioBuffer) {
+		this.segmentsReaders = [];
+		this.audioBuffer = audioBuffer;
+
+		this.setSegmentsNumber(segmentsNumber);
+		this.setSize(size);
+	};
+
+	Shape.prototype.size = null;
+	Shape.prototype.segmentsNumber = null;
+	Shape.prototype.segmentsReaders = null;
+	Shape.prototype.audioBuffer = null;
+
+	Shape.prototype.setSize = function(size) {
+		this.size = size;
+
+		return this;
+	};
+
+	Shape.prototype.setSegmentsNumber = function(number) {
+		this.segmentsNumber = Math.max(1, number);
+		arrayResize(this.segmentsReaders, this.segmentsNumber);
+
+		return this;
+	};
+
+	Shape.prototype.setSegmentReader = function(index, reader) {
+		index = Math.max(0, Math.min(index, this.segmentsNumber - 1));
+		this.segmentsReaders[index] = reader;
+
+		return this;
+	};
 
 	App.Shape = Shape;
 })(App || {});
