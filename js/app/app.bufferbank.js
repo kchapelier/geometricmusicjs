738687a52691a9e1766440c52ebc46ec444833b2
kchapelier <kevin.chapelier@gmail.com>
2014-07-12 12:45:46 +0200

    test sample loading

--- js/app/app.bufferbank.js
+++ js/app/app.bufferbank.js
@@ -3,5 +3,16 @@
 
 	var BufferBank = null;
 
+	Aural.Utils.XHR.parallelLoad(
+		[
+			{ url : './samples/test.wav', 'type' : 'buffer', 'id' : 'test' },
+			{ url : './samples/test2.wav', 'type' : 'buffer', 'id' : 'test2' },
+		],
+		null,
+		function(buffers) {
+			console.log(buffers);
+		}.bind(this)
+	);
+
 	App.BufferBank = BufferBank;
 })(App || {});
