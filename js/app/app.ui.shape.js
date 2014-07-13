2160cca2f19f6a32534a1923619d9849598cd4d8
kchapelier <kevin.chapelier@gmail.com>
2014-07-13 09:27:02 +0200

    externalize shape UI

---
+++ js/app/app.ui.shape.js
@@ -0,0 +1,64 @@
+(function(App) {
+	"use strict";
+
+	var ShapeSvg = function(width, size, segments) {
+		this.svg = new Snap(width, width);
+		this.width = width;
+		this.size = size;
+		this.segments = segments;
+		this.refresh();
+	};
+
+	ShapeSvg.prototype.createShape = function() {
+		var w = this.width,
+			p = this.segments,
+			c = w / 2,
+			r = (this.width - 1) / (this.size + 1);
+
+		var shape;
+
+		if(p === 1) {
+			shape = this.svg.circle(c, c, r - c / 5);
+		} else if(p === 2) {
+			//TODO dunno what to draw
+		} else {
+			var points = [];
+
+			r = r - 3 * p - 3;
+
+			for (var cp = 0; cp < p; cp++) {
+				points.push(c + r * Math.sin(cp * Math.PI / p * 2));
+				points.push(c + r * Math.cos(cp * Math.PI / p * 2));
+			}
+
+			shape = this.svg.polygon(points);
+		}
+
+		return shape;
+	};
+
+	ShapeSvg.prototype.refresh = function() {
+		this.shape = this.createShape().attr({
+			fill: "#000",
+			fillOpacity : 0,
+			stroke: "#000",
+			strokeWidth: 2
+		});
+
+		this.shape.animate({
+			fillOpacity: 1
+		}, 500);
+	};
+
+	var Shape = function(width, size, segments) {
+		this.element = document.createElement('div');
+		this.element.className = 'shape';
+		this.element.style.width = width + 'px';
+		this.element.style.height = width + 'px';
+		this.svg = new ShapeSvg(width, size, segments);
+		this.element.appendChild(this.svg.svg.node);
+	};
+
+	App.UI = App.UI || {};
+	App.UI.Shape = Shape;
+})(App || {});
